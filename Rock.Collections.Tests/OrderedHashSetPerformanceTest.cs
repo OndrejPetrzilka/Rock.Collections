914e070a75a3fddd7547e087bae5c537bae8f132
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2016-10-21 18:46:15 +0200

    + OrderedDictionary, tests
    + OrderedHashSet, tests

---
+++ Rock.Collections.Tests/OrderedHashSetPerformanceTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rock.Collections;
+
+namespace Rock.Tests.Base
+{
+    [TestClass]
+    public class OrderedHashSetPerformanceTest
+    {
+        static readonly int IterCount = 2000000;
+        static IEnumerable<int> m_warm = Enumerable.Range(0, IterCount);
+
+        static HashSet<int> m_warmHashSet = new HashSet<int>(m_warm);
+        static OrderedHashSet<int> m_warmOrderedSet = new OrderedHashSet<int>(m_warm);
+
+        static HashSet<int> m_warmHashSetFull = new HashSet<int>(m_warm);
+        static OrderedHashSet<int> m_warmOrderedSetFull = new OrderedHashSet<int>(m_warm);
+
+        static OrderedHashSetPerformanceTest()
+        {
+            m_warmHashSet.Clear();
+            m_warmOrderedSet.Clear();
+
+            new OrderedHashSetPerformanceTest().TestAddHashSet();
+            new OrderedHashSetPerformanceTest().TestAddOrderedHashSet();
+        }
+
+        [TestMethod]
+        public void TestAddHashSet()
+        {
+            m_warmHashSet.Clear();
+            for (int i = 0; i < IterCount; i++)
+            {
+                m_warmHashSet.Add(i);
+            }
+        }
+
+        [TestMethod]
+        public void TestAddOrderedHashSet()
+        {
+            m_warmOrderedSet.Clear();
+            for (int i = 0; i < IterCount; i++)
+            {
+                m_warmOrderedSet.Add(i);
+            }
+        }
+
+        [TestMethod]
+        public void TestRemoveHashSet()
+        {
+            for (int i = 0; i < IterCount; i++)
+            {
+                m_warmHashSetFull.Remove(i);
+            }
+        }
+
+        [TestMethod]
+        public void TestRemoveOrderedHashSet()
+        {
+            for (int i = 0; i < IterCount; i++)
+            {
+                m_warmOrderedSetFull.Remove(i);
+            }
+        }
+    }
+}
---
+++ Rock.Collections.Tests/OrderedHashSetTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rock.Collections;
+
+namespace Rock.Collections.Tests
+{
+    [TestClass]
+    public class OrderedHashSetTest
+    {
+        class Both
+        {
+            public LinkedList<int> List = new LinkedList<int>();
+            public OrderedHashSet<int> Set = new OrderedHashSet<int>();
+
+            public bool EnableTrim = true;
+
+            public int[] CopiedData
+            {
+                get
+                {
+                    int[] data = new int[Set.Count];
+                    Set.CopyTo(data);
+                    return data;
+                }
+            }
+
+            private void Validate()
+            {
+                Assert.IsTrue(Enumerable.SequenceEqual(List, Set));
+                Assert.IsTrue(Enumerable.SequenceEqual(List.Reverse(), Set.Reversed));
+                Assert.IsTrue(Enumerable.SequenceEqual(List, CopiedData));
+                if (EnableTrim)
+                {
+                    Set.TrimExcess();
+                    Assert.IsTrue(Enumerable.SequenceEqual(List, Set));
+                    Assert.IsTrue(Enumerable.SequenceEqual(List.Reverse(), Set.Reversed));
+                    Assert.IsTrue(Enumerable.SequenceEqual(List, CopiedData));
+                }
+            }
+
+            public Both Add(int item) { if (!List.Contains(item)) { List.AddLast(item); } Set.Add(item); Validate(); return this; }
+            public Both Remove(int item) { List.Remove(item); Set.Remove(item); Validate(); return this; }
+            public Both Contains(int item) { Assert.IsTrue(List.Contains(item) == Set.Contains(item)); return this; }
+            public Both MoveFirst(int item) { if (List.Remove(item)) List.AddFirst(item); Set.MoveFirst(item); Validate(); return this; }
+            public Both MoveLast(int item) { if (List.Remove(item)) List.AddLast(item); Set.MoveLast(item); Validate(); return this; }
+            public Both MoveBefore(int item, int mark) { if (item != mark && List.Contains(mark) && List.Remove(item)) List.AddBefore(List.Find(mark), item); Set.MoveBefore(item, mark); Validate(); return this; }
+            public Both MoveAfter(int item, int mark) { if (item != mark && List.Contains(mark) && List.Remove(item)) List.AddAfter(List.Find(mark), item); Set.MoveAfter(item, mark); Validate(); return this; }
+        }
+
+        Both New { get { return new Both(); } }
+        Both NoTrim { get { return new Both() { EnableTrim = false }; } }
+        Both One { get { return new Both().Add(1); } }
+        Both Two { get { return new Both().Add(1).Add(2); } }
+        Both Four { get { return new Both().Add(1).Add(2).Add(3).Add(4); } }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            New.Add(1).Add(2).Add(3).Add(3);
+        }
+
+        [TestMethod]
+        public void TestAddRemove()
+        {
+            New.Add(1).Add(2).Add(3).Remove(2).Add(4).Contains(2).Contains(3);
+            New.Add(1).Add(2).Add(3).Remove(2).Add(4).Remove(4).Remove(3).Remove(1).Remove(2);
+            NoTrim.Add(1).Add(2).Add(3).Remove(2).Add(4).Remove(4).Remove(3).Remove(1).Remove(2).Add(5);
+            var x = NoTrim;
+            for (int i = 0; i < 100; i++)
+            {
+                x.Add(i);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                x.Remove(i);
+            }
+        }
+
+        [TestMethod]
+        public void TestMove()
+        {
+            One.MoveFirst(1);
+            One.MoveLast(1);
+            One.MoveFirst(2);
+            One.MoveLast(2);
+
+            Two.MoveFirst(1);
+            Two.MoveLast(1);
+            Two.MoveFirst(2);
+            Two.MoveLast(2);
+
+            Two.MoveAfter(1, 2);
+            Two.MoveAfter(2, 1);
+            Two.MoveBefore(1, 2);
+            Two.MoveBefore(2, 1);
+
+
+            Four.MoveFirst(1).MoveFirst(2).MoveFirst(3).MoveFirst(4).MoveFirst(6);
+            Four.MoveFirst(4).MoveFirst(3).MoveFirst(2).MoveFirst(1);
+            Four.MoveLast(1).MoveLast(2).MoveLast(3).MoveLast(4).MoveLast(6);
+            Four.MoveLast(4).MoveLast(3).MoveLast(2).MoveLast(1);
+
+            Four.MoveBefore(1, 2).MoveBefore(2, 1).MoveBefore(4, 3).MoveBefore(4, 3);
+            Four.MoveAfter(1, 2).MoveAfter(2, 1).MoveAfter(4, 3).MoveAfter(4, 3);
+            Four.MoveAfter(1, 4);
+            Four.MoveAfter(4, 1);
+            Four.MoveBefore(4, 1);
+            Four.MoveBefore(1, 4);
+
+            Four.MoveAfter(1, 1);
+            Four.MoveAfter(4, 4);
+            Four.MoveBefore(1, 1);
+            Four.MoveBefore(4, 4);
+
+            Four.MoveAfter(1, 6);
+            Four.MoveAfter(6, 1);
+            Four.MoveBefore(1, 6);
+            Four.MoveBefore(6, 1);
+        }
+    }
+}
